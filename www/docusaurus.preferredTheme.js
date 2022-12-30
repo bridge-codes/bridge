3d514d90c0fa25d38551870099d11c7ff323e03f
raulmihaila <raul.mihaila01@gmail.com>
2022-12-30 19:42:02 +0100

    Update docusaurus.preferredTheme.js

--- www/docusaurus.preferredTheme.js
+++ www/docusaurus.preferredTheme.js
@@ -8,7 +8,7 @@ if (ExecutionEnvironment.canUseDOM) {
   const htmlElement = document.querySelector('html');
 
   const setInitialTheme = () => {
-    const newTheme = mediaMatch.matches ?  darkTheme : lightTheme;
+    const newTheme = mediaMatch.matches ?  darkTheme : darkTheme;
     htmlElement?.setAttribute('data-theme', newTheme);
   };
   setInitialTheme();
