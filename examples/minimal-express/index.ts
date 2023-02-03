6eee62e2fe45fd6cbcfb9f3aee982752beb23e72
Digitalu <87531803+digitalu@users.noreply.github.com>
2023-02-03 10:48:54 +0100

    package bridge removed url

--- examples/minimal-express/index.ts
+++ examples/minimal-express/index.ts
@@ -6,7 +6,7 @@ const routes = { hello: handler({ resolve: () => 'hello' }) };
 
 const app = express();
 
-app.use('', initBridge({ routes, url: 'http://localhost:8080' }).expressMiddleware());
+app.use('', initBridge({ routes }).expressMiddleware());
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- packages/bridge/source/bridge.ts
+++ packages/bridge/source/bridge.ts
@@ -5,13 +5,12 @@ import type * as express from 'express';
 import { createHttpHandler } from './server/adapters/node-http';
 import http from 'http';
 
-class Bridge<Routes extends BridgeRoutes, URL extends string> {
+class Bridge<Routes extends BridgeRoutes> {
   public bridgeType!: RoutesToBridgeType<Routes>;
 
   constructor(
     public routes: Routes,
     private config: { errorHandler?: ErrorHandler; formidable?: any },
-    public url: URL,
   ) {}
 
   public expressMiddleware = (): express.Handler => createHttpHandler(this.routes, this.config);
@@ -19,14 +18,12 @@ class Bridge<Routes extends BridgeRoutes, URL extends string> {
   public HTTPServer = () => http.createServer(createHttpHandler(this.routes, this.config));
 }
 
-export const initBridge = <Routes extends BridgeRoutes, URL extends string>({
+export const initBridge = <Routes extends BridgeRoutes>({
   routes,
-  url,
   errorHandler,
   formidable,
 }: {
   routes: Routes;
-  url?: URL;
   formidable?: any;
   errorHandler?: ErrorHandler;
-}): Bridge<Routes, URL> => new Bridge(routes, { formidable, errorHandler }, url as any);
+}): Bridge<Routes> => new Bridge(routes, { formidable, errorHandler });
